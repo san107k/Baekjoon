7248e3993dc965534de01ea596cc2835fd5f2c31
san107k <123722952+san107k@users.noreply.github.com>
2023-01-28 21:11:36 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a, b;
+
+	while (1) {
+		scanf("%d %d", &a, &b);
+		if (a > 0 && b > 0 && a < 10 && b < 10)
+			break;
+	}
+	printf("%d", a * b);
+
+	return 0;
+}
