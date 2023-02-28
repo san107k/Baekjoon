94513a8ada63247db56689d4f119f0606ab1bb30
san107k <123722952+san107k@users.noreply.github.com>
2023-03-01 02:22:41 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char text[101] = {0,};
+    char text[101] = {0};
 
-    while (scanf("%[^\n]s", text) != EOF) {
+    while (scanf("%[^\n]", text) != EOF) {
         getchar();
         printf("%s\n", text);
     }
